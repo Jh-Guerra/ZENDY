a659f194ba0739b0bac50ce3bff57aee7cfea9ec
Kevin Valencia <kevinvalenciadiaz18@gmail.com>
2021-05-27 22:20:07 -0500

    CHAT - CUERPO | Rediseñar el cuerpo de los mensajes

--- src/pages/chat_page/Components/ChatItem.js
+++ src/pages/chat_page/Components/ChatItem.js
@@ -11,11 +11,11 @@ const ChatItem = props => {
       className={`chat__item ${user ? user : ""}`}
     >
       <div className="chat__item__content">
-        <div className="chat__msg">{message}</div>
         <div className="chat__meta">
-          <span>16 mins ago</span>
-          <span>Seen 1.03PM</span>
+          <span>{user}</span>
+          <span>12:05 PM</span>
         </div>
+        <div className="chat__msg">{message}</div>
       </div>
       <ChatAvatar isOnline="active" image={image} />
     </div>
