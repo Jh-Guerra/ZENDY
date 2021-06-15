f452a26d4568e66fe18f6a1ff97987bc96bdc016
Edu Estula <edu.estula@ext.blaze.me>
2021-06-14 20:34:52 -0500

    Define Custom Buttons

--- src/assets/styles/zendy-css.js
+++ src/assets/styles/zendy-css.js
@@ -1,4 +1,4 @@
-export const pColor = "#2F103D";
+export const pColor = "#303e7a";
 export const sColor = "#F39651";
 // export const sColor = "white";
 export const sColorL = "#E68B6A";
--- src/components/CustomButtom.js
+++ src/components/CustomButtom.js
@@ -1,21 +1,27 @@
-import { ButtonBase, Tooltip} from '@material-ui/core';
+import { Button, ButtonBase, Tooltip, withStyles} from '@material-ui/core';
+import { purple } from '@material-ui/core/colors';
 import React from 'react';
+import { LightenDarkenColor } from 'utils/common';
 
 const CustomButton = props => {
-    const { content=false, styleButton, iconLeft, iconRight, marginRight, marginLeft, svgRight, svgLeft, ...other} = props;
-    return(
-        <div className={props.styleButton}>
-            <Tooltip title={props.nameTooltip || ""} arrow>
-                <ButtonBase disabled={props.disabled} style={{marginLeft: props.margin ? props.margin : props.marginLeft, marginRight: props.margin ? props.margin : props.marginRight}}  {...other}>
-                    { svgLeft? <div className="blaze-icons" style={{marginRight:'-4px', marginLeftt:'-4px'}}> {props.svgLeft} </div> : null}
-                    {props.iconLeft ? <span className={`blaze-icons ${props.iconLeft}`} /> : null}
-                    {props.content ? <> {props.children} </>: null}
-                    {props.iconRight? <span className={`blaze-icons ${props.iconRight}`} /> : null}
-                    { svgRight? <div className="blaze-icons" style={{marginLeft:'-4px', marginRight:'-4px'}}> {props.svgRight} </div> : null}
-                </ButtonBase>
-            </Tooltip>
-        </div>
-    )
+    const { children, startIcon, size, variant, color } = props;
+
+    const ColorButton = withStyles((theme) => ({
+        root: {
+        color: theme.palette.getContrastText(color),
+            backgroundColor: color,
+            '&:hover': {
+                backgroundColor: LightenDarkenColor(color, 25),
+            },
+            '&:disabled': {
+                backgroundColor: LightenDarkenColor(color, 40),
+            },
+        },
+    }))(Button);
+
+    return <ColorButton {...props} startIcon={startIcon && startIcon} >
+        {children}
+    </ColorButton>;
 }
 
 export default CustomButton
