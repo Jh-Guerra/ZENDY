19838b58d3c6d1d4a74784f73d64ea376393c26a
Edu Estula <edu.estula@ext.blaze.me>
2021-08-18 18:23:24 -0500

    configure pusher

--- src/pages/test_page/BlankPage.js
+++ src/pages/test_page/BlankPage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import BasePage from 'components/BasePage';
 import ZendyIcon from '../../assets/images/zendy-icon.jpg'
 import Echo from "laravel-echo";
+window.Pusher = require('pusher-js')
 
 class BlankPage extends Component {
   constructor(props) {
@@ -14,7 +15,7 @@ class BlankPage extends Component {
   async componentDidMount() {
     window.Echo = new Echo({
       broadcaster: "pusher",
-      key: 'ASDASD2121',
+      key: 'ZENDY_PUSHER_KEY',
       wsHost: window.location.hostname,
       wsPort: 6001,
       forceTLS: false,
