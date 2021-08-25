b1f6aa0b6ba8e3ed2131ebb04568af12e09e9f2a
Gerson Salazar <eduardosv04@gmail.com>
2021-08-24 23:04:40 -0500

    correccion de la visualizacion de los emojis e implementacion de envio

--- src/pages/main_page/MainPage.js
+++ src/pages/main_page/MainPage.js
@@ -3,8 +3,7 @@ import BasePage from 'components/BasePage';
 import { Grid } from "@material-ui/core";
 import MainHeader from "pages/main_page/Childrens/MainHeader";
 import MainBody from "pages/main_page/Childrens/MainBody";
-//import MainFooter from "pages/main_page/Childrens/MainFooter";
-
+import MainFooter from "pages/main_page/Childrens/MainFooter";
 class MainPage extends Component {
 
   constructor(props) {
@@ -28,12 +27,12 @@ class MainPage extends Component {
         <Grid item xs={12} style={{height:'13vh'}}>
           <MainHeader/>
         </Grid>
-        <Grid item xs={12} style={{height:'74vh'}}>
+        <Grid item xs={12} style={{height:'13vh'}}>
           <MainBody/>
         </Grid>
-        {/* <Grid item xs={12} style={{height:'13vh'}}>
+        <Grid item xs={12} style={{height:'74vh'}}>
           <MainFooter/>
-        </Grid> */}
+        </Grid>
       </Grid>
     );
   }
